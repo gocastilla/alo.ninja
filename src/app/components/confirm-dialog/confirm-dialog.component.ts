fa08c7968e19ab17e5e54ce0c0f135f698a77f92
Álvaro Quirós <alvaro.quiros@gmail.com>
2018-08-22 10:59:33 +0200

    fix: close modal after click
    closes #7

--- src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,18 +1,17 @@
-import {Component, Inject} from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'confirm-dialog',
-  templateUrl: 'confirm-dialog.html',
+  templateUrl: 'confirm-dialog.html'
 })
 export class ConfirmDialog {
-
   constructor(
     public dialogRef: MatDialogRef<ConfirmDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
